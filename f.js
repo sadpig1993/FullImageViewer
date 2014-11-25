8d49440ea20516a776aeab957b0625acbe798065
溪空 <xikong.xjw@alibaba-inc.com>
2014-11-25 12:39:21 +0800

    creat demo test

---
+++ f.js
@@ -0,0 +1,22 @@
+/*
+ * FullImageViewer 0.1
+ *
+ * Sadpig
+ * Copyright 2014, MIT License
+ *
+*/
+
+
+function f(options){
+     "use strict";
+      var o = options;
+      if(!o.url){
+      	return;
+      }
+
+      function loader(){
+      	var  o= new Image();  
+             o.src = url;  
+      }
+
+}
