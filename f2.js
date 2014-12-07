0973f3bac536ed9d4e4d1c07dca3781ee5268990
溪空 <xikong.xjw@alibaba-inc.com>
2014-12-08 03:42:15 +0800

    css3

--- f2.js
+++ f2.js
@@ -46,7 +46,7 @@ function f(container, options) {
 		y: 0,
 		frame: 0
 	}
-	var ticker, v, amp, target, isTouchstart, img, acc;
+	var ticker, v, amp, target, isTouchstart,img;
 
 	var isScrolling;
 
@@ -55,17 +55,11 @@ function f(container, options) {
 		imgHeight = img.parentNode.offsetHeight;
 		max = getMax();
 		if (browser.touch) {
-			element.addEventListener('webkitTransitionEnd', events, false);
-			element.addEventListener('msTransitionEnd', events, false);
-			element.addEventListener('oTransitionEnd', events, false);
-			element.addEventListener('otransitionend', events, false);
-			element.addEventListener('transitionend', events, false);
 			element.addEventListener('touchstart', events, false);
 			element.addEventListener('click', events, false);
 		}
 
 	});
-
 	var events = {
 		handleEvent: function(event) {
 			//console.log(event);
@@ -83,24 +77,18 @@ function f(container, options) {
 					break;
 				case 'click':
 					this.close(events);
-					break;
-				case 'webkitTransitionEnd':
-				case 'msTransitionEnd':
-				case 'oTransitionEnd':
-				case 'otransitionend':
-				case 'transitionend':
-					this.transitionEnd(event);
-					break;
+					//case 'webkitTransitionEnd':
+					//case 'msTransitionEnd':
+					//case 'oTransitionEnd':
+					//case 'otransitionend':
+					//case 'transitionend': offloadFn(this.transitionEnd(event)); break;
 					//case 'resize': offloadFn(setup); break;
 			}
 		},
 		start: function(event) {
-			element.style.webkitTransform = getComputedStyle(element).webkitTransform;
-			console.log();
-			this.transitionEnd();
 			isScrolling = false;
 			if (opt.inertialScrolling) {
-				ticker = setInterval(track, 16);
+				ticker = setInterval(track, 33);
 			}
 			var touches = event.touches[0];
 			start = {
@@ -130,96 +118,43 @@ function f(container, options) {
 
 			isScrolling = true;
 			//console.log(delta.y);
-			var goY = pos.y + delta.y;
-			console.log(goY);
-			console.log(pos.y+'+'+delta.y);
 
-			if(goY > min){
-				if(delta.y == goY -min){
-					goY = pos.y + delta.y*0.4;
-				}else{
-					goY = min + (goY -min)*0.4;
-				}
-				
-			}
-			if(goY < max){
-				if(Math.abs(delta.y) == Math.abs(max - goY)){
-					goY = pos.y+ delta.y*0.5;
-				}else{
-					goY = max + (goY -max)*0.4;
-				}
-			}
-			console.log(goY);
-			translate(element,goY , 0, 122);
+			translate(element, pos.y + delta.y, 0, 122);
 
 
 		},
 		end: function(event) {
 			clearInterval(ticker);
-
 			if (!isScrolling) return;
 			pos.x = pos.x + delta.x
 			pos.y = pos.y + delta.y;
-			if (opt.inertialScrolling) {
-				var z = 0.0015;
-				var o, p;
-				v > 2 && (v = 2), -2 > v && (v = -2);
-				v >= 0 && (o = z), 0 > v && (o = z * -1);
-			             p = i({
-					v: v,
-					a: -o
-				});
-				console.log(p);
-				//acc = v < 0 ? 20 : -20; // 加速度
-				console.log("v:" + v);
-				console.log(o);
-				amp = -(v * v) / (2 * -o);
-				var time = p.t.toFixed(2);
-				target = Math.round(pos.y + amp);
-				if (target <= max) {
-					target = max;
-					amp = target - pos.y;
-				} else if (target >= min) {
-					target = min;
-					amp = target - pos.y;
+			if (opt.inertialScrolling ) {
+
+				if (v > 10 || v < -10 || pos.y < max || pos.y > min) {
+					amp = 0.3 * v;
+					console.log("amp:"+amp);
+					console.log("pos.y:"+pos.y);
+					target = Math.round(pos.y + amp);
+
+					if (target <= max) {
+						target = max;
+						amp = target - pos.y;
+					} else if (target >= min) {
+						target = min;
+						amp = target - pos.y;
+					}
+					console.log('target:'+target);
+					//target = Math.round(pos.y+amp) < -1531 ? -1531 : Math.round(pos.y+amp);
+					start.time = Date.now();
+					requestAnimationFrame(autoScroll);
+
 				}
-				if (target == min && pos.y > min || target == max && pos.y < max) {
-					time = 0.4;
-				}
-				console.log("amp:" + amp)
-				console.log("time:" + time);
-				console.log("target");
-				element.style.webkitTransition = '-webkit-transform ' + time + 's cubic-bezier(0.33, 0.66, 0.66, 1)';
-				translate(element, target, time, 178);
-				pos.y = target;
 			}
 
 		},
 		close: function(event) {
 			con.hide();
-		},
-		transitionEnd: function(event) {
-			///pos.y = h(element);
-			var tempPos = h(element);
-			pos.y = tempPos.y;
-			pos.x = tempPos.x
-			element.style.webkitTransition =
-				element.style.msTransition =
-				element.style.oTransition =
-				element.style.MozTransition =
-				element.style.transition = '';
-			opt.transitionEnd && opt.transitionEnd.call(event);
-
 		}
-	}
-
-	function h(t) {
-		var e, i = {
-				x: 0,
-				y: 0
-			},
-			n = getComputedStyle(t).webkitTransform;
-		return "none" !== n && (e = n.match(/^matrix3d\((?:[-\d.]+,\s*){12}([-\d.]+),\s*([-\d.]+)(?:,\s*[-\d.]+){2}\)/) || n.match(/^matrix\((?:[-\d.]+,\s*){4}([-\d.]+),\s*([-\d.]+)\)$/)) && (i.x = parseFloat(e[1]) || 0, i.y = parseFloat(e[2]) || 0), i
 	}
 
 	function getMax() {
@@ -249,17 +184,12 @@ function f(container, options) {
 
 	function translate(ele, dist, speed, t) {
 		if (!isScrolling) return;
+		console.log(t);
 		var slide = ele;
 		var style = slide && slide.style;
 
 		if (!style) return;
-		if (speed) {
-			style.webkitTransitionDuration =
-				style.MozTransitionDuration =
-				style.msTransitionDuration =
-				style.OTransitionDuration =
-				style.transitionDuration = speed + 's';
-		}
+
 
 
 		style.webkitTransform = 'translateY(' + dist + 'px)';
@@ -273,19 +203,14 @@ function f(container, options) {
 		now = Date.now();
 		elapsed = now - start.time; //其实这里近似就是100ms了
 		start.time = now; //更新时间戳
-		deltaY = (delta.y - pos.frame);
+
+		deltaY = delta.y - pos.frame;
 		pos.frame = delta.y; //计算偏移,offset仍然在scroll()函 数中被更新
-		v =  deltaY / (elapsed+0.1); //防止分母为0的情况
+		v = 1000 * deltaY / (1 + elapsed); //防止分母为0的情况
 		//console.log(v);
 	}
 
-	function i(data) {
-		var v, a, t, s;
-	             t = -data.v/data.a;
-		return {
-			t:t/1000
-		}
-	}
+
 
 	function loader(url, callback) {
 		var o = new Image();
